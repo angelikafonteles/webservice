769d4ade2c2eb9d0ddcbec090052f5ca2768ae30
Angelika Fonteles <angelikatf@gmail.com>
2020-11-18 15:33:12 -0300

    Payment, one-to-one association

--- src/main/java/com/angelikafonteles/webservice/config/TestConfig.java
+++ src/main/java/com/angelikafonteles/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.angelikafonteles.webservice.entities.Category;
 import com.angelikafonteles.webservice.entities.Order;
 import com.angelikafonteles.webservice.entities.OrderItem;
+import com.angelikafonteles.webservice.entities.Payment;
 import com.angelikafonteles.webservice.entities.Product;
 import com.angelikafonteles.webservice.entities.User;
 import com.angelikafonteles.webservice.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
