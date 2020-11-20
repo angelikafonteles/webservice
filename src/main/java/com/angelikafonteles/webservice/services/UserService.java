f6fce2542779d081ec8ecedd3acc8dc3f40f5947
Angelika Fonteles <angelikatf@gmail.com>
2020-11-20 15:48:30 -0300

    Exception handling - update

--- src/main/java/com/angelikafonteles/webservice/services/UserService.java
+++ src/main/java/com/angelikafonteles/webservice/services/UserService.java
@@ -3,6 +3,8 @@ package com.angelikafonteles.webservice.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +46,13 @@ public class UserService {
 		
 	
 	public User update(Long id, User obj) {
+		try{
 		User entity = repo.getOne(id);
 		updateData(entity, obj);
 		return repo.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new  ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
