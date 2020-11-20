ecb582b6dd81c709f8f033cea59ed2deae48fd71
Angelika Fonteles <angelikatf@gmail.com>
2020-11-20 14:56:40 -0300

    Exception handling - delete

--- src/main/java/com/angelikafonteles/webservice/services/UserService.java
+++ src/main/java/com/angelikafonteles/webservice/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.angelikafonteles.webservice.entities.User;
 import com.angelikafonteles.webservice.repositories.UserRepository;
+import com.angelikafonteles.webservice.services.exceptions.DatabaseException;
 import com.angelikafonteles.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,8 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repo.deleteById(id);
+		try {
+			repo.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
+		
 	
 	public User update(Long id, User obj) {
 		User entity = repo.getOne(id);
